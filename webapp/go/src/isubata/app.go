71df5e5baa7a773d8af6e7cdf2e3bec802c1e177
chikuwa111 <take0shun1111@gmail.com>
2018-08-31 08:16:51 +0000

    Comment out

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -95,7 +95,7 @@ type User struct {
 
 func getUser(userID int64) (*User, error) {
 	u := User{}
-	if err := db.Get(&u, "SELECT * FROM user WHERE id = ?", userID); err != nil {
+	if err := db.Get(&u, "SELECT id, name, display_name, avatar_icon FROM user WHERE id = ?", userID); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil
 		}
@@ -114,20 +114,20 @@ func addMessage(channelID, userID int64, content string) (int64, error) {
 	return res.LastInsertId()
 }
 
-type Message struct {
-	ID        int64     `db:"id"`
-	ChannelID int64     `db:"channel_id"`
-	UserID    int64     `db:"user_id"`
-	Content   string    `db:"content"`
-	CreatedAt time.Time `db:"created_at"`
-}
+// type Message struct {
+// 	ID        int64     `db:"id"`
+// 	ChannelID int64     `db:"channel_id"`
+// 	UserID    int64     `db:"user_id"`
+// 	Content   string    `db:"content"`
+// 	CreatedAt time.Time `db:"created_at"`
+// }
 
-func queryMessages(chanID, lastID int64) ([]Message, error) {
-	msgs := []Message{}
-	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
-		lastID, chanID)
-	return msgs, err
-}
+// func queryMessages(chanID, lastID int64) ([]Message, error) {
+// 	msgs := []Message{}
+// 	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
+// 		lastID, chanID)
+// 	return msgs, err
+// }
 
 func sessUserID(c echo.Context) int64 {
 	sess, _ := session.Get("session", c)
@@ -350,21 +350,21 @@ func postMessage(c echo.Context) error {
 	return c.NoContent(204)
 }
 
-func jsonifyMessage(m Message) (map[string]interface{}, error) {
-	u := User{}
-	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
-		m.UserID)
-	if err != nil {
-		return nil, err
-	}
-
-	r := make(map[string]interface{})
-	r["id"] = m.ID
-	r["user"] = u
-	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
-	r["content"] = m.Content
-	return r, nil
-}
+// func jsonifyMessage(m Message) (map[string]interface{}, error) {
+// 	u := User{}
+// 	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
+// 		m.UserID)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 
+// 	r := make(map[string]interface{})
+// 	r["id"] = m.ID
+// 	r["user"] = u
+// 	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+// 	r["content"] = m.Content
+// 	return r, nil
+// }
 
 func getMessage(c echo.Context) error {
 	userID := sessUserID(c)
@@ -438,26 +438,26 @@ func queryChannels() ([]int64, error) {
 	return res, err
 }
 
-func queryHaveRead(userID, chID int64) (int64, error) {
-	type HaveRead struct {
-		UserID    int64     `db:"user_id"`
-		ChannelID int64     `db:"channel_id"`
-		MessageID int64     `db:"message_id"`
-		UpdatedAt time.Time `db:"updated_at"`
-		CreatedAt time.Time `db:"created_at"`
-	}
-	h := HaveRead{}
-
-	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
-		userID, chID)
-
-	if err == sql.ErrNoRows {
-		return 0, nil
-	} else if err != nil {
-		return 0, err
-	}
-	return h.MessageID, nil
-}
+// func queryHaveRead(userID, chID int64) (int64, error) {
+// 	type HaveRead struct {
+// 		UserID    int64     `db:"user_id"`
+// 		ChannelID int64     `db:"channel_id"`
+// 		MessageID int64     `db:"message_id"`
+// 		UpdatedAt time.Time `db:"updated_at"`
+// 		CreatedAt time.Time `db:"created_at"`
+// 	}
+// 	h := HaveRead{}
+// 
+// 	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
+// 		userID, chID)
+// 
+// 	if err == sql.ErrNoRows {
+// 		return 0, nil
+// 	} else if err != nil {
+// 		return 0, err
+// 	}
+// 	return h.MessageID, nil
+// }
 
 func fetchUnread(c echo.Context) error {
 	userID := sessUserID(c)
@@ -513,7 +513,7 @@ func getHistory(c echo.Context) error {
 
 	const N = 20
 	var cnt int64
-	err = db.Get(&cnt, "SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?", chID)
+	err = db.Get(&cnt, "SELECT COUNT(channel_id) as cnt FROM message WHERE channel_id = ?", chID)
 	if err != nil {
 		return err
 	}
